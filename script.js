952858dba8bb5ce855cc618e00ecc79235fe0be2
knreano <112625935+knreano@users.noreply.github.com>
2022-12-19 13:41:08 +0800

    add custom cursor

---
+++ script.js
@@ -0,0 +1,28 @@
+let cursorInner = document.querySelector(".cursor-inner");
+let cursorOuter = document.querySelector(".cursor-outer");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(event) {
+  let x = event.clientX;
+  let y = event.clientY;
+
+  console.log(x, y);
+  cursorInner.style.left = x + "px";
+  cursorInner.style.top = y + "px";
+  cursorOuter.style.left = x + "px";
+  cursorOuter.style.top = y + "px";
+}
+
+let links = Array.from(document.querySelectorAll(".navbar a"));
+links.forEach(link => {
+  console.log(links);
+  link.addEventListener("mouseover", () => {
+    cursorInner.classList.add("grow");
+    cursorOuter.classList.add("grow");
+  });
+  link.addEventListener("mouseleave", () => {
+    cursorInner.classList.remove("grow");
+    cursorOuter.classList.remove("grow");
+  });
+})
